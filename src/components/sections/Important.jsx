dad8fa96f653e3be3f87570742338d5660fd6c34
Edison Matos <edison.matos@live.com>
2026-06-18 17:49:45 -0300

    feat: Atualiza links

--- src/components/sections/Important.jsx
+++ src/components/sections/Important.jsx
@@ -61,8 +61,22 @@ function Important({ colorMode }) {
         <MotionDivDownUp>
           <ButtonReflexo
             label={content.texts.important.ctaButtonText}
-            icon={content.texts.svgs.wpp}
-            link={content.texts.links.ctaWhatsapp}
+            icon=<svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-corner-down-right-icon lucide-corner-down-right"
+            >
+              <path d="m15 10 5 5-5 5" />
+              <path d="M4 4v7a4 4 0 0 0 4 4h12" />
+            </svg>
+            link="https://pay.hotmart.com/Y105799270M?bid=1781201184158"
           />
         </MotionDivDownUp>
       </SectionWrapper>
